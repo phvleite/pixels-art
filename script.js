a9cdafdca1a4f1f23e35f31a8b6b8b0c82260a47
phvleite <phvleite@gmail.com>
2021-12-13 19:08:18 -0300

    iniciado o projeto

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    let paleta = document.getElementById('color-palette')
+
+    for (i = 1; i < 5; i += 1) {
+        let paletaCor = document.createElement('div');
+        paletaCor.setAttribute("id", `paleta${i}`);
+        paletaCor.setAttribute("class", "color");
+        paleta.appendChild(paletaCor);
+    }
+    let paletasCores = document.getElementsByClassName('color');
+
+    for (i = 0; i < paletasCores.length; i += 1) {
+        if (i === 0) {
+            paletasCores[i].style.backgroundColor = 'Black';
+        } else {
+            let corPaleta = `rgb(${parseInt(Math.random()*256)},${parseInt(Math.random()*256)},${parseInt(Math.random()*256)})`
+            paletasCores[i].style.backgroundColor = corPaleta;
+        }
+    }
+}
